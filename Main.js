8ca1d4852e8f4770e11e7a8c4f9670c8a8dd3774
oxmc <67136658+oxmc@users.noreply.github.com>
2021-01-27 04:06:56 -0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,6 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const MusicBot = require('discord-music-system');
+
+const bot = new MusicBot({
+    botPrefix: config.prefix,
+    ytApiKey: config.ytapikey,
+    botClient: client 
+});
+
+client.on('message', message => { // When the bot receive a message
+    if(message.content.startsWith(bot.prefix)) { // If the message starts with your prefix
+        bot.onMessage(message); // The music-system must read the message, to check if it is a music command and execute it.
+    };
+});
 
 client.on('ready', () =>{
     client.user.setStatus('Online')
