b749cb35149177f16f5e8797ebca614c86117423
oxmc <67136658+oxmc@users.noreply.github.com>
2021-01-27 04:02:23 -0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,4 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
 
+client.on('ready', () =>{
+    client.user.setStatus('Online')
+    console.log('Connected!')
+})
+//rest of your code
+
+//always remember to never share your token with anyone
+client.login(config.token)
 
 function Restart() {
 setTimeout(function(){ 
