464d1a6905895708d02a6b2f0d9f488add33ab79
oxmc <67136658+oxmc@users.noreply.github.com>
2021-01-29 14:57:57 -0800

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+echo installing...
+echo open this in a browser: https://nodejs.org/dist/v14.15.4/node-v14.15.4-x64.msi
+set /p DUMMY=Hit ENTER to continue...
+npm install discord.js
+npm i discord-music-system
+start.bat
