e8c258d36329a51f633f09625afd396dae01ff5f
oxmc <67136658+oxmc@users.noreply.github.com>
2021-03-07 02:49:52 -0800

    Update installpi.sh

--- installpi.sh
+++ installpi.sh
@@ -1,5 +1,11 @@
 echo "installing..."
-sudo apt install nodejs
+#Delete .bat files as they do not run on linux...
+rm -rf install.bat
+rm -rf start.bat
+echo "Installing nodejs and required packages..."
+sudo apt install nodejs -y
 npm install discord.js
 npm i discord-music-system
+npm i nodemon
+echo "Starting bot..."
 bash start.sh
