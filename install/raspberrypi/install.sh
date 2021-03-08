75e2360c06424ff1ed769b25fd3dfd83e54f0034
oxmc <67136658+oxmc@users.noreply.github.com>
2021-03-08 14:03:26 -0800

    Update install.sh

--- install/raspberrypi/install.sh
+++ install/raspberrypi/install.sh
@@ -1,6 +1,7 @@
 echo "installing..."
 echo "Installing nodejs and required packages..."
 sudo apt install nodejs -y
+npm install
 npm install discord.js
 npm install --save discord-player
 npm install --save @discordjs/opus
